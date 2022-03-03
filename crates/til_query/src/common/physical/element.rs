71de3838aaeca67f42df71405c36d02f4127276f
matthijsr <matthijsr@users.noreply.github.com>
2022-03-03 15:53:55 +0100

    Everything is still fields, as not all logical fields become part of the same physical stream.

--- crates/til_query/src/common/physical/element.rs
+++ crates/til_query/src/common/physical/element.rs
@@ -1,23 +1,8 @@
 use super::fields::Fields;
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum ElementType<T: Clone + PartialEq> {
-    Null,
-    Bits(T),
-    Group(Fields<T>),
-    Union(UnionElement<T>),
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct UnionElement<T: Clone + PartialEq> {
-    fields: Fields<T>,
-    data: T,
-    tag: T,
-}
-
-#[derive(Debug, Clone, PartialEq)]
 pub struct Element<T: Clone + PartialEq> {
-    data: ElementType<T>,
+    data: Fields<T>,
     last: Option<T>,
     stai: Option<T>,
     endi: Option<T>,
