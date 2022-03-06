c6c462a84146070ae652e20b9161ce6e03dac66c
matthijsr <matthijsr@users.noreply.github.com>
2022-03-06 10:46:20 +0100

    Test object assignment

--- crates/vhdl/src/architecture/arch_storage/object_queries.rs
+++ crates/vhdl/src/architecture/arch_storage/object_queries.rs
@@ -10,8 +10,13 @@ use super::interner::Interner;
 
 #[salsa::query_group(ObjectStorage)]
 pub trait ObjectQueries: Interner {
+    /// Verify whether two object types can be assigned to one another
     fn assignable_types(&self, left: Id<ObjectType>, right: Id<ObjectType>) -> Result<()>;
 
+    /// Verify whether `from` can be assigned to `to`
+    fn assignable_objects(&self, to: ObjectKey, from: ObjectKey) -> Result<()>;
+
+    /// Get an object based on its key
     fn get_object(&self, key: ObjectKey) -> Result<Object>;
 }
 
@@ -32,6 +37,16 @@ impl ObjectKey {
 
     pub fn selection(&self) -> &Vec<FieldSelection> {
         &self.selection
+    }
+
+    pub fn with_selection(mut self, selection: FieldSelection) -> Self {
+        self.selection.push(selection);
+        self
+    }
+
+    pub fn with_nested(mut self, mut selection: Vec<FieldSelection>) -> Self {
+        self.selection.append(&mut selection);
+        self
     }
 }
 
@@ -58,4 +73,13 @@ fn get_object(db: &dyn ObjectQueries, key: ObjectKey) -> Result<Object> {
         typ: db.intern_object_type(typ),
         assignable: obj.assignable,
     })
+}
+
+fn assignable_objects(db: &dyn ObjectQueries, to: ObjectKey, from: ObjectKey) -> Result<()> {
+    let to = db.get_object(to)?;
+    to.assignable.to_or_err()?;
+    let from = db.get_object(from)?;
+    from.assignable.from_or_err()?;
+
+    db.assignable_types(to.typ, from.typ)
 }
--- crates/vhdl/src/object/mod.rs
+++ crates/vhdl/src/object/mod.rs
@@ -1,4 +1,5 @@
 use object_type::ObjectType;
+use tydi_common::error::{Error, Result};
 use tydi_intern::Id;
 
 pub mod array;
@@ -12,6 +13,30 @@ pub struct Assignable {
     pub to: bool,
     /// Can be assigned from
     pub from: bool,
+}
+
+impl Assignable {
+    /// If `to` is false, returns an error
+    pub fn to_or_err(&self) -> Result<()> {
+        if self.to {
+            Ok(())
+        } else {
+            Err(Error::InvalidTarget(
+                "The selected object cannot be assigned to".to_string(),
+            ))
+        }
+    }
+
+    /// If `from` is false, returns an error
+    pub fn from_or_err(&self) -> Result<()> {
+        if self.from {
+            Ok(())
+        } else {
+            Err(Error::InvalidTarget(
+                "The selected object cannot be assigned from".to_string(),
+            ))
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Copy)]
