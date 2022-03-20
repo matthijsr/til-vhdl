e5806525d535c8c6f3e28a5f82309e52d3dd1b5e
matthijsr <matthijsr@users.noreply.github.com>
2022-03-20 16:50:20 +0100

    Create a default physical transfer

--- crates/til_query/src/common/transfer/physical_transfer.rs
+++ crates/til_query/src/common/transfer/physical_transfer.rs
@@ -18,6 +18,9 @@ pub enum LastMode {
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 /// The method by which `last` is transferred.
 pub enum StrobeMode {
+    /// This stream has no Dimensionality or Complexity >= 8,
+    /// so does not require a strobe signal.
+    None,
     /// This stream has Complexity < 8, so asserts `strobe` per transfer.
     Transfer(bool),
     /// This stream has Complexity >= 8, so can assert `strobe` per element lane.
@@ -49,37 +52,121 @@ pub struct PhysicalTransfer {
     /// The maximum width of each element being transferred.
     max_element_size: NonNegative,
     /// The data being transfered, organized by lane.
-    data: Vec<Vec<bool>>,
+    ///
+    /// When None, treat all lanes as active, but drive all '0's.
+    ///
+    /// When Some, but empty or short of the number of element lanes, do not
+    /// drive the unaddressed lanes.
+    ///
+    /// When Some, and certain elements are shorter than the maximum element
+    /// size, shift to align to the LSB and do not drive the unaddressed bits.
+    data: Option<Vec<Vec<bool>>>,
     /// The dimensionality supported by the physical stream.
     dimensionality: NonNegative,
     /// The `last` signalling for the transfer.
-    last_mode: LastMode,
+    last: LastMode,
     /// The index of the first active lane.
+    ///
+    /// Requires: C≥6∧N>1
     ///
     /// May not be N or greater.
     ///
     /// When C < 6, must always be 0.
-    start_index: NonNegative,
+    start_index: Option<NonNegative>,
     /// The index of the last active lane.
+    ///
+    /// Requires (C≥5∨D≥1)∧N>1
     ///
     /// * May not be 0.
     /// * May not be N or greater.
     /// * May not be less than `start_index`.
     ///
     /// When C < 5, and `last` is zero, end index must be N-1.
-    end_index: Positive,
+    end_index: Option<Positive>,
     /// The `strb` signal.
     ///
-    /// At C < 8, this is used to indicate whether the transfer is empty.
-    /// At C >= 8, this indicates the activity of individual element lanes.
+    /// Requires: C≥7∨D≥1
+    ///
+    /// At C < 7, this is used to indicate whether the transfer is empty.
+    /// At C >= 7, this indicates the activity of individual element lanes.
+    ///
+    /// NOTE: This is an assumption, the original spec claims the cut-off is
+    /// C >= 8. But this makes C=7 effectively useless.
     strobe: StrobeMode,
     /// The maximum width of the `user` signal being transferred.
     max_user_size: NonNegative,
     /// The `user` signal
-    user: Vec<bool>,
+    ///
+    /// When None, drive all '0's.
+    user: Option<Vec<bool>>,
 }
 
 impl PhysicalTransfer {
+    /// Creates a default, base transfer based on the given properties.
+    ///
+    /// This transfer will always be valid for the given properties (barring
+    /// `user`-based constraints).
+    pub fn new(
+        complexity: Complexity,
+        element_lanes: Positive,
+        max_element_size: NonNegative,
+        dimensionality: NonNegative,
+        max_user_size: NonNegative,
+    ) -> Self {
+        let last_mode = if dimensionality >= 1 {
+            if complexity >= Complexity::new_major(8) {
+                LastMode::Lane(vec![
+                    Some(0..(dimensionality - 1));
+                    element_lanes.get().try_into().unwrap()
+                ])
+            } else {
+                LastMode::Transfer(Some(0..(dimensionality - 1)))
+            }
+        } else {
+            LastMode::None
+        };
+
+        let element_lanes_gt_1 = element_lanes > Positive::new(1).unwrap();
+
+        let start_index = if element_lanes_gt_1 && complexity >= Complexity::new_major(6) {
+            Some(0)
+        } else {
+            None
+        };
+
+        let end_index = if element_lanes_gt_1
+            && (dimensionality >= 1 || complexity >= Complexity::new_major(5))
+        {
+            Positive::new(element_lanes.get() - 1)
+        } else {
+            None
+        };
+
+        let strobe = if complexity >= Complexity::new_major(7) {
+            StrobeMode::Lane(vec![true; element_lanes.get().try_into().unwrap()])
+        } else if dimensionality >= 1 {
+            StrobeMode::Transfer(true)
+        } else {
+            StrobeMode::None
+        };
+
+        Self {
+            complexity,
+            is_empty_sequence: false,
+            holds_valid: false,
+            element_lanes,
+            max_element_size,
+            data: None,
+            dimensionality,
+            last: last_mode,
+            start_index,
+            end_index,
+            strobe,
+            max_user_size,
+            user: None,
+        }
+    }
+
     /// The Complexity this transfer adheres to. This value informs whether the
     /// logical transfer is valid.
     pub fn complexity(&self) -> &Complexity {
@@ -116,7 +203,15 @@ impl PhysicalTransfer {
     }
 
     /// The data being transfered, organized by lane.
-    pub fn data(&self) -> &Vec<Vec<bool>> {
+    ///
+    /// When None, treat all lanes as active, but drive all '0's.
+    ///
+    /// When Some, but empty or short of the number of element lanes, do not
+    /// drive the unaddressed lanes.
+    ///
+    /// When Some, and certain elements are shorter than the maximum element
+    /// size, shift to align to the LSB and do not drive the unaddressed bits.
+    pub fn data(&self) -> &Option<Vec<Vec<bool>>> {
         &self.data
     }
 
@@ -126,34 +221,43 @@ impl PhysicalTransfer {
     }
 
     /// The `last` signalling for the transfer.
-    pub fn last_mode(&self) -> &LastMode {
-        &self.last_mode
+    pub fn last(&self) -> &LastMode {
+        &self.last
     }
 
     /// The index of the first active lane.
     ///
+    /// Requires: C≥6∧N>1
+    ///
     /// May not be N or greater.
     ///
     /// When C < 6, must always be 0.
-    pub fn start_index(&self) -> NonNegative {
-        self.start_index
+    pub fn start_index(&self) -> &Option<NonNegative> {
+        &self.start_index
     }
 
     /// The index of the last active lane.
+    ///
+    /// Requires (C≥5∨D≥1)∧N>1
     ///
     /// * May not be 0.
     /// * May not be N or greater.
     /// * May not be less than `start_index`.
     ///
     /// When C < 5, and `last` is zero, end index must be N-1.
-    pub fn end_index(&self) -> Positive {
-        self.end_index
+    pub fn end_index(&self) -> &Option<Positive> {
+        &self.end_index
     }
 
     /// The `strb` signal.
     ///
-    /// At C < 8, this is used to indicate whether the transfer is empty.
-    /// At C >= 8, this indicates the activity of individual element lanes.
+    /// Requires: C≥7∨D≥1
+    ///
+    /// At C < 7, this is used to indicate whether the transfer is empty.
+    /// At C >= 7, this indicates the activity of individual element lanes.
+    ///
+    /// NOTE: This is an assumption, the original spec claims the cut-off is
+    /// C >= 8. But this makes C=7 effectively useless.
     pub fn strobe(&self) -> &StrobeMode {
         &self.strobe
     }
@@ -164,7 +268,9 @@ impl PhysicalTransfer {
     }
 
     /// The `user` signal
-    pub fn user(&self) -> &Vec<bool> {
+    ///
+    /// When None, drive all '0's.
+    pub fn user(&self) -> &Option<Vec<bool>> {
         &self.user
     }
 }
