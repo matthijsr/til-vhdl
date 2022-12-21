d17179fc4f4a5c7a84153c6c6af558e500f4ee0c
matthijsr <matthijsr@users.noreply.github.com>
2022-12-21 19:27:52 +0100

    WIP test util

--- crates/til_query/src/test_utils.rs
+++ crates/til_query/src/test_utils.rs
@@ -3,6 +3,7 @@ use tydi_intern::Id;
 use crate::{
     common::{
         logical::logicaltype::{
+            genericproperty::GenericProperty,
             stream::{Stream, Synchronicity, Throughput},
             LogicalType,
         },
@@ -10,7 +11,10 @@ use crate::{
     },
     ir::{
         db::Database,
-        generics::{behavioral::integer::IntegerGeneric, GenericParameter},
+        generics::{
+            behavioral::integer::IntegerGeneric, interface::dimensionality::DimensionalityGeneric,
+            GenericParameter,
+        },
         implementation::{structure::Structure, Implementation},
         physical_properties::InterfaceDirection,
         streamlet::Streamlet,
@@ -21,7 +25,7 @@ use crate::{
 
 use tydi_common::{
     error::{Result, TryResult},
-    name::PathName,
+    name::{Name, PathName},
     numbers::NonNegative,
 };
 
@@ -132,5 +136,41 @@ pub fn simple_structural_streamlet_with_behav_params(
             GenericParameter::try_new("pb", IntegerGeneric::positive(), 2)?,
             GenericParameter::try_new("pc", IntegerGeneric::integer(), -2)?,
         ],
+    )
+}
+
+pub fn simple_streamlet_with_interface_params(
+    db: &mut Database,
+    name: impl TryResult<PathName>,
+) -> Result<Streamlet> {
+    let bits = LogicalType::try_new_bits(4)?.intern(db);
+    let data_type = LogicalType::try_new_union(None, vec![("a", bits), ("b", bits)])?.intern(db);
+    let null_type = LogicalType::null_id(db);
+    let generic_dim_param: GenericProperty<NonNegative> = Name::try_new("pa")?.into();
+    let stream = Stream::try_new(
+        db,
+        data_type,
+        1.0,
+        generic_dim_param + GenericProperty::Fixed(1),
+        Synchronicity::Sync,
+        4,
+        StreamDirection::Forward,
+        null_type,
+        false,
+    )?;
+    let streamlet = Streamlet::new().try_with_name(name)?.with_ports(
+        db,
+        vec![
+            ("a", stream, InterfaceDirection::In),
+            ("b", stream, InterfaceDirection::Out),
+        ],
+    )?;
+    streamlet.with_parameters(
+        db,
+        vec![GenericParameter::try_new(
+            "pa",
+            DimensionalityGeneric::new(),
+            5,
+        )?],
     )
 }
